0f5471565cf0e2c6caed3486141a3607aaf61761
Nazla44 <nazlaramadhani115@gmail.com>
2025-06-13 20:36:39 +0700

    menambahkan data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,12 @@ $routes->group('kategori-produk', ['filter' => 'auth'], function ($routes) {
     $routes->get('delete/(:any)', 'KategoriProdukController::delete/$1');
 });
 
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+
+$routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
+$routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
+
 $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     $routes->get('', 'TransaksiController::index');
     $routes->post('', 'TransaksiController::cart_add');
